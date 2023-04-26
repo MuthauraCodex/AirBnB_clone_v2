abc2758f567df394103815599bfc802f2556571a
MuthauraCodex <gwenivymuthaura@gmail.com>
2023-04-26 13:04:03 +0000

    AirBnB_clone_v2

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
 """
+Script that  starts Flask app
+Listens on 0.0.0.0 port 5000
+"""
+from flask import Flask, render_template
 from models import storage
-from flask import Flask
-from flask import render_template
+from models.state import State
+import models
+
 
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
-
-    States are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
-
-
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def teardown_session(self):
+    """tear down the sqlalchemy session """
     storage.close()
 
 
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """displays all states"""
+    states = models.storage.all(State)
+    list_of_states = [state for state in states.values()]
+    return render_template("7-states_list.html",
+                           list_of_states=list_of_states)
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0:5000")
+    app.run(host='0.0.0.0', port=5000)
