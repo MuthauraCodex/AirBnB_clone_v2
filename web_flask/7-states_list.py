2f8543d04897e8b004a546e51830119dab87e3f3
MuthauraCodex <gwenivymuthaura@gmail.com>
2023-04-26 15:43:14 +0000

    AirBnB_clone_v2

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""Starts a flask app
-    listens to 0.0.0.0:5000
+"""Starts a Flask web application.
 
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /states_list: HTML page with a list of all State objects in DBStorage.
 """
-from models import storage
+from .models import storage
 from flask import Flask
 from flask import render_template
 
@@ -13,6 +15,7 @@ app = Flask(__name__)
 @app.route("/states_list", strict_slashes=False)
 def states_list():
     """Displays an HTML page with a list of all State objects in DBStorage.
+
     States are sorted by name.
     """
     states = storage.all("State")
